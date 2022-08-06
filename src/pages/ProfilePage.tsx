78c3d6730f9fc92f707297a8455a08adb15ca92d
loicj <loic.jacquel.fr@gmail.com>
2022-08-06 13:51:49 +0900

    fixing auth0 namespace

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,27 +1,30 @@
 import React, { useEffect } from 'react';
 import { useAuth0, User } from '@auth0/auth0-react';
 
-interface TodoUser extends User {
-  username?: string;
-}
-
 export default function ProfilePage() {
-  const { user, isAuthenticated, isLoading } = useAuth0<TodoUser>();
+  const { user, isAuthenticated, isLoading } = useAuth0<User>();
+
+  useEffect(() => {}, [isAuthenticated]);
 
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
+  if (!isAuthenticated) {
+    return <div>Please log in ...</div>;
+  }
+
+  if (!user) {
+    return <div>Something went wrong, please try again ...</div>;
+  }
+
   return (
-    <>
-      {!isAuthenticated && 'Please log in ...'}
-      {isAuthenticated && (
-        <div>
-          <img src={user?.picture} alt={user?.name} />
-          <h2>Welcome {user?.username}</h2>
-          <p>{user?.email}</p>
-        </div>
-      )}
-    </>
+    <div>
+      <img src={user.picture} alt={user.name} />
+      <h2>
+        Welcome {user[`${process.env.REACT_APP_AUTH0_NAMESPACE}username`]}
+      </h2>
+      <p>{user.email}</p>
+    </div>
   );
 }
